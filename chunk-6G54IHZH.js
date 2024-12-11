0cdbb79f185082e311367a294248ad60380b461d
Nestor Martínez <cdcreanimartesas@gmail.com>
2024-12-10 21:37:48 -0500

    Auto-generated commit

--- chunk-6G54IHZH.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-EQDQRRRY.js";var o=[{path:"",loadComponent:()=>import("./chunk-ZNC6GNUH.js").then(t=>t.CertificateFormComponent)}];export{o as certificateRoutes};
---
+++ chunk-LEM4DOQH.js
@@ -0,0 +1,1 @@
+import"./chunk-EQDQRRRY.js";var o=[{path:"",loadComponent:()=>import("./chunk-AK2E44SR.js").then(t=>t.CertificateFormComponent)}];export{o as certificateRoutes};
