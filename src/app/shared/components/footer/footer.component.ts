94bb84d1e392aa317d326da134579177dd6cb0e2
Nestor Martínez <cdcreanimartesas@gmail.com>
2025-01-21 10:08:45 -0500

    ✨ Update version to 1.0.0; enhance styling and layout for certificate components and footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as packageJson from './../../../../../package.json';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  version = packageJson.version;
   currentYear = new Date().getFullYear();
 }
